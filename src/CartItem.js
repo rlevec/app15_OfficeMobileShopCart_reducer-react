66cea725ce318870abd7f5a30dd60d07e6944e5f
rlevec <rene.levec@outlook.com>
2022-08-18 10:12:39 +0200

    init

--- src/CartItem.js
+++ src/CartItem.js
@@ -4,7 +4,7 @@ import {faAngleUp} from '@fortawesome/free-solid-svg-icons'
 import {faAngleDown} from '@fortawesome/free-solid-svg-icons'
 import { useGlobalContext } from './context'
 const CartItem = ({ id, img, title, price, amount}) => {
-  const {removeItem, increaseAmount, decreaseAmount, toggleAmount} = useGlobalContext()
+  const {removeItem, toggleAmount} = useGlobalContext()
   return (
     <article className='cart-item'>
       <img src={img} alt={title} className='item-img'/>
